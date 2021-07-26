239cd2cf176be051ecad434fc24e0f2ed5034d11
Franciele Fonseca <franciele.fonseca@linx.com.br>
2021-07-26 19:58:32 -0300

    classroom js 26 julho

---
+++ 01_PrimeirosPassosLinguagemJS/02_trabalhandoComVariaveis.js
@@ -0,0 +1,7 @@
+const nome = "Lais";
+const Sobrenome = "Silva"
+const Idade = 30;
+const anoDeNascimento = 1993;
+
+//"A falta de padronização entre os nomes das variáveis pode gerar muitos problemas no futuro da aplicação 
+//e os programadores não saberão quando usar cada tipo de nomenclatura"
---
+++ 01_PrimeirosPassosLinguagemJS/04_conversoesDeTipos.js
@@ -0,0 +1,4 @@
+console.log("ano" + 2020); // ano 2020
+console.log("2" + "2"); // 22
+
+console.log(parseInt("2") + parseInt("2")); // 4
